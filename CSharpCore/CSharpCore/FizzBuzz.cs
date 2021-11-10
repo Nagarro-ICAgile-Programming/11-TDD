e6fd245755a1591bcb09efc7ade3d0d33827764a
Daniel Konrad <daniel.konrad@admiral.at>
2021-11-10 14:51:25 +0100

    initial commit

---
+++ CSharpCore/CSharpCore/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpCore
+{
+    public static class FizzBuzz
+    {
+        private const string fizz = "Fizz";
+        private const string buzz = "Buzz";
+        private const string fizzbuzz = "FizzBuzz";
+
+        public static string Answer(int input)
+        {
+            if (input % 15 == 0)
+            {
+                return fizzbuzz;
+            }
+            if (input % 3 == 0)
+            {
+                return fizz;
+            }
+            if (input % 5 == 0)
+            {
+                return buzz;
+            }
+
+            return input.ToString();
+        }
+    }
+}
---
+++ CSharpCore/CSharpCoreTest/FizzBuzzTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpCore.Test
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Answer_ReturnsNumber()
+        {
+            string result = FizzBuzz.Answer(1);
+
+            result.Should().Be("1");
+        }
+
+        [Fact]
+        public void Answer_ReturnsFizz_WhenDivisibleOnlyBy3()
+        {
+            string result = FizzBuzz.Answer(3);
+
+            result.Should().Be("Fizz");
+        }
+
+        [Fact]
+        public void Answer_ReturnsBuzz_WhenDivisibleOnlyBy5()
+        {
+            string result = FizzBuzz.Answer(5);
+
+            result.Should().Be("Buzz");
+        }
+
+        [Fact]
+        public void Answer_ReturnsFizzBuzz_WhenDivisibleBy3And5()
+        {
+            string result = FizzBuzz.Answer(15);
+
+            result.Should().Be("FizzBuzz");
+        }
+    }
+}
