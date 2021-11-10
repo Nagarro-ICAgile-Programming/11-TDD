5618c562ae409f7a0587b493ea3faca3324d974a
Daniel Konrad <daniel.konrad@admiral.at>
2021-11-10 15:13:34 +0100

    Started TDD

---
+++ CSharpCore/CSharpCore/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpCore
+{
+    public static class Calculator
+    {
+        public static int Add(string input)
+        {
+            if (input.Equals(string.Empty) || input.Equals(","))
+            {
+                return 0;
+            }
+            if (!int.TryParse(input, out var result))
+            {
+                throw new ArgumentException();
+            }
+
+            return result;
+        }
+    }
+}
--- CSharpCore/CSharpCoreTest/CalculatorTests.cs
+++ CSharpCore/CSharpCoreTest/CalculatorTests.cs
@@ -1,10 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
+using Xunit;
 
 namespace CSharpCore.Test
 {
-    class CalculatorTests
+    public class CalculatorTests
     {
+        [Fact]
+        public void Add_Return0_WhenInputIsEmpty()
+        {
+            int result = Calculator.Add("");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Add_ReturnInput_WhenInputIsNumber()
+        {
+            int result = Calculator.Add("1");
+
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Add_ThrowArgumentException_WhenInputInvalid()
+        {
+            Action action = new Action(() => Calculator.Add("invalid"));
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Add_Return0_WhenInputContainsEmptyDelimiter()
+        {
+            int result = Calculator.Add(",");
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Add_Return0_WhenInputContainsMultipleEmptyDelimiter()
+        {
+            int result = Calculator.Add(@",\n");
+
+            result.Should().Be(0);
+        }
     }
 }
+
+
+/* TODO list 
+
+[X] "" => 0
+[X] "2" => 2
+[X] "invalid" => ArgumentException
+[X] "," => 0
+[ ] ",\n" => 0
+[ ] ",2" oder "2," => 2
+[ ] "1,2" => 3
+[ ] "1,2\n3" => 6
+[ ] "1,,2" => 3
+[ ] " 1,  34 " => 35
+[ ] "1, 2, a" => ArgumentException
+[ ] "//+\n 4+5+6" => 15
+[ ] "//+\n 4\n5,6" => 15
+[ ] "//0\n 4+5+6" => ArgumentException
+[ ] "//\n1" => ArgumentException
+[ ] "//*\n 4+5+6" => ArgumentException
+[ ] "//-\n 4-5-6" => 15
+
+*/
