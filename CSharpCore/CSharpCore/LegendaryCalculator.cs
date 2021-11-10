da91292c4b9ad08dfba61f6c373c9ab8a2e28cd6
3012954 <jan.pawlus@admiral.at>
2021-11-10 11:33:22 +0100

    legendary calculator implementation

---
+++ CSharpCore/CSharpCore/LegendaryCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpCore
+{
+    public static class LegendaryCalculator
+    {
+        public static int Add(string input)
+        {
+            try
+            {
+                List<char> delimiters = new List<char> { '\n', ',' };
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    return 0;
+                }
+                if (input.StartsWith("//") && input[3] == '\n')
+                {
+                    delimiters.Add(input[2]);
+                    input = input.Substring(4);
+                }
+                var stringNumbers = input.Split(delimiters.ToArray());
+                return Array.ConvertAll(stringNumbers, int.Parse).Sum();
+            }
+            catch
+            {
+                throw new ArgumentException($"{input} is a fucking mess");
+            }
+                                    
+        }
+    }
+}
---
+++ CSharpCore/CSharpCoreTest/LegendaryCalculatorTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharpCore.Test
+{
+    public class LegendaryCalculatorTest
+    {
+        [Fact]
+        public void ShouldReturnZeroFromEmptyString()
+        {
+            LegendaryCalculator.Add(string.Empty).Should().Be(0);
+        }
+
+        [Fact]
+        public void ShouldReturOneNumberFromOneNumberString()
+        {
+            LegendaryCalculator.Add("1").Should().Be(1);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("1\n2", 3)]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("1 \n 2 , 3", 6)]
+        public void ShouldAddNumbersWithSpecifiedDelimiters(string inputString, int resultNumber)
+        {
+            LegendaryCalculator.Add(inputString).Should().Be(resultNumber);
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentException()
+        {
+            Action exceptionAdd = () => LegendaryCalculator.Add("Xd");
+            exceptionAdd.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ShouldAddNumbersWithCustomDelimiter()
+        {
+            LegendaryCalculator.Add("//|\n1|1\n1,1").Should().Be(4);
+        }
+    }
+}
