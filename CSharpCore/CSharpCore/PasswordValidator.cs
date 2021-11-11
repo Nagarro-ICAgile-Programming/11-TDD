3dc48c5b79a19f709d725db429de9e7f598c9247
Daniel Zauner <daniel.zauner@admiral.at>
2021-11-11 10:43:08 +0100

    added password validator and its test

---
+++ CSharpCore/CSharpCore/PasswordValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace CSharpCore
+{
+    public class PasswordValidator
+    {
+        private static char[] specialCharacters = new char[] { '_', '-', ',', '/' };
+
+        public bool Validate(string password)
+        {
+            if (password.Length < 6 || password.Length > 12)
+                return false;
+
+            if (!password.Any(char.IsLower) || !password.Any(char.IsUpper) || !password.Any(char.IsDigit))
+                return false;
+
+            return password.Any(c => specialCharacters.Contains(c));
+        }
+    }
+}
---
+++ CSharpCore/CSharpCoreTest/PasswordValidatorTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharpCore.Test
+{
+    public class PasswordValidatorTest
+    {
+        // • A password must be at least 6 characters long
+        // • It must contain at least a lowercase character
+        // • It must contain at least an uppercase character
+        // • It must contain at least a number
+        // • It must contain at least one: underscore, dash, comma or slash.
+        // • It must be at most 12 characters long
+        
+        // "123" -> false // length
+        // "1234567890aA_" -> false // length
+        // "123aA_" -> true // underscore
+        // "123aA-" -> true // dash
+        // "123aA," -> true // comma
+        // "123aA/" -> true // slash
+        // "1234A_" -> false // no lowercase
+        // "1234a_" -> false // no uppercase
+        // "aA_aA_" -> false // no number
+        
+        [Theory]
+        [InlineData("1234A_")]
+        [InlineData("1234a_")]
+        [InlineData("aA_aA_")]
+        [InlineData("123")]
+        [InlineData("1234567890aA_")]
+        public void PasswordValidator_Validate_ShouldBeInvalid(string password)
+        {
+            var validator = new PasswordValidator();
+            var result = validator.Validate(password);
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("123aA_")]
+        [InlineData("123aA-")]
+        [InlineData("123aA,")]
+        [InlineData("123aA/")]
+        public void PasswordValidator_Validate_ShouldBeValid(string password)
+        {
+            var validator = new PasswordValidator();
+            var result = validator.Validate(password);
+            result.Should().BeTrue();
+        }
+    }
+}
