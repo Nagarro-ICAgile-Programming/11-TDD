f4f02f76af95c975851f034cb9ea52cd7a2197bd
Daniel Pucher <D.Pucher@admiral.at>
2021-11-11 10:48:20 +0100

    add password validator

---
+++ CSharpCore/CSharpCore/PasswordValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCore
+{
+    public class PasswordValidator
+    {
+        private static readonly List<char> specialCharacters = new List<char> { '_', '-', ',', '/' };
+
+        public static bool Validate(string password)
+        {
+            return
+                password.Length >= 6 &&
+                password.Any(p => char.IsLower(p)) &&
+                password.Any(p => char.IsUpper(p)) &&
+                password.Any(p => char.IsDigit(p)) &&
+                password.Any(p => specialCharacters.Contains(p)) &&
+                password.Length <= 12;
+        }
+    }
+}
---
+++ CSharpCore/CSharpCoreTest/PasswordValidatorTEst.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharpCore.Test
+{
+    public class PasswordValidatorTest
+    {
+        [Theory]
+        [InlineData("aA123456_")]
+        [InlineData("aA123456-")]
+        [InlineData("aA123456,")]
+        [InlineData("aA123456/")]
+        public void ShouldBeValid(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("aA1_")]
+        public void ShouldContainAtLeast6Characters(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("A123456_")]
+        public void ShouldContainAtLeast1LowercaseCharacter(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("a123456_")]
+        public void ShouldContainAtLeast1UppercaseCharacter(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("aAaaa_")]
+        public void ShouldContainAtLeast1Number(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("aA123456")]
+        public void ShouldContainAtLeast1UnderscoreDashCommaOrSlash(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("aA0123456789_")]
+        public void ShouldContainAtMost12Characters(string password)
+        {
+            PasswordValidator.Validate(password).Should().BeFalse();
+        }
+    }
+}
+
