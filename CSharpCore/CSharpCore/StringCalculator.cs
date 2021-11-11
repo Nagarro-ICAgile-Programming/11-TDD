8346e675116f5eeff083e8dc3c1f8fa37ece6d6b
Rosegger Rene <Rene.Rosegger@admiral.at>
2021-11-11 09:20:48 +0100

    stringcalculator changes

---
+++ CSharpCore/CSharpCore/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpCore
+{
+    public static class StringCalculator
+    {
+        private static char[] delimiter = new char[] { '\n', ',' };
+
+        public static int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            if (input.Contains('-'))
+            {
+                throw new Exception("Negative numbers are not allowed");
+            }
+
+            input = ReplaceCustomDelimiter(input);
+
+            string[] numbers = input.Split(delimiter);
+
+            return numbers.Aggregate(0, (sum, i) =>
+                {
+                    int.TryParse(i, out var result);
+                    return sum + result;
+                });
+
+        }
+
+        private static string ReplaceCustomDelimiter(string input)
+        {
+            if (input.StartsWith("//"))
+            {
+                char customDelimiter = input[2];
+                input = input.Replace(customDelimiter, delimiter[0]);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ CSharpCore/CSharpCoreTest/StringCalculatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpCore.Test
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void ShouldReturnZeroIfEmpty()
+        {
+            StringCalculator.Add(string.Empty).Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void ShouldReturnStringAsNumber(string input, int expectedNumber)
+        {
+            StringCalculator.Add(input).Should().Be(expectedNumber);
+        }
+
+        [Theory]
+        [InlineData("1\n2", 3)]
+        [InlineData("1\n2,3", 6)]
+        public void ShouldSumStringWithDelimiter(string input, int expectedNumber)
+        {
+            StringCalculator.Add(input).Should().Be(expectedNumber);
+        }
+
+        [Theory]
+        [InlineData(" 2, 5", 7)]
+        public void ShouldIgnoreWidespaces(string input, int expectedNumber)
+        {
+            StringCalculator.Add(input).Should().Be(expectedNumber);
+        }
+
+        [Theory]
+        [InlineData("//+\n 3+4", 7)]
+        public void ShouldUseCustomDelimiter(string input, int expectedNumber)
+        {
+            StringCalculator.Add(input).Should().Be(expectedNumber);
+        }
+
+        [Theory]
+        [InlineData("-1,-4")]
+        public void ShouldThrowException(string input)
+        {
+            Action action = () => StringCalculator.Add(input);
+            action.Should().Throw<Exception>().WithMessage("Negative numbers are not allowed");
+        }
+    }
+}
